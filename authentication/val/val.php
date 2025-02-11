39ee0d19d81f6463a5d36d2e940cb6775f10ca42
Jonn-Ian <jonniansabar@gmail.com>
2025-02-11 15:23:16 +0800

    Update val.php

--- authentication/val/val.php
+++ authentication/val/val.php
@@ -25,3 +25,10 @@ function login($conn, $username, $password){
         return false;
     }
 }
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    
+    login($conn, $username, $password);
+}
