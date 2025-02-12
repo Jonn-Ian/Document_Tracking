abd2a466651832aaecb61eddb21b2754b6320b91
Dave <59647186+s1v-byte@users.noreply.github.com>
2025-02-12 11:41:01 +0800

    Employee page WIP

--- auth/conn/conn.php
+++ auth/conn/conn.php
@@ -1,11 +1,11 @@
 <?php
 
 $servername = "localhost";
-$username = "root";
+$ServerUsername = "root";
 $ServerPassword = "";
 $db_name = "docu_tracker";
 
-$conn = new mysqli($servername, $username, $ServerPassword, $db_name);
+$conn = new mysqli($servername, $ServerUsername, $ServerPassword, $db_name);
 
 if ($conn->connect_error) {
     die('Failed to connect to MySQL: ' . $conn->connect_error);
